0782ca4421155457191aecb2c54052a731e49718
Jakob <jakob.sterri@gmail.com>
2023-04-30 00:38:23 +0200

    fixed port issue

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ import os
 
 # If file is called directly called, then run the app on the PORT provided defined in ENV or use '6969'.
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=os.getenv('PORT', 6969))
+    app.run()
 
