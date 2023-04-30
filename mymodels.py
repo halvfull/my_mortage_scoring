a117b28ba71349e7517f09fa3ca2adee74667154
Jakob <jakob.sterri@gmail.com>
2023-04-30 13:08:10 +0200

    updated customer model.

--- mymodels.py
+++ mymodels.py
@@ -3,7 +3,7 @@ from app import db
 
 class Customer(db.Model):
   customerSSN = db.Column(db.Integer, primary_key=True)
-  fullName = db.Column(db.String, unique=True, nullable=False)
+  fullName = db.Column(db.String, nullable=False)
   loanAmount  = db.Column(db.Float, nullable=False)
   equityAmount = db.Column(db.Float, nullable=False)
   salaryAmount = db.Column(db.Float, nullable=False)
