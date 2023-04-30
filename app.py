00e728112f47c1389ad3604f617cc921cd96c3bb
Jakob <jakob.sterri@gmail.com>
2023-04-30 13:35:29 +0200

    simple front-end

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 # Define app.
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///MortageScoringDB"
 db = SQLAlchemy(app)
 
 
--- modules/register_customer.py
+++ modules/register_customer.py
@@ -1,6 +1,6 @@
 # Import Libraries 
 from app import app, db
-from flask import request
+from flask import request, render_template
 from mymodels import Customer
 
 #checks if user exists in DB
@@ -8,7 +8,8 @@ def get_user_by_ssn(customerSSN):
    customer = Customer.query.filter_by(customerSSN=customerSSN).first()
    return customer
 
-
+''' 
+#old routing without front-end
 @app.route('/register', methods= ['GET', 'POST'])
 def register_customer():
    customerSSN = request.args.get('customerSSN')
@@ -25,3 +26,29 @@ def register_customer():
       db.session.add(cust)
       db.session.commit()
       return f"Sucessfully registered new user: {cust.fullName}  with ssn: {cust.customerSSN}"
+'''
+
+@app.route('/register-form')
+def register_form():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def registerer_cust():
+    if request.method == 'POST':
+        customerSSN = request.form['customerSSN']
+        fullName = request.form['fullName']
+        loanAmount = request.form['loanAmount']
+        equityAmount = request.form['equityAmount']
+        salaryAmount = request.form['salaryAmount']
+
+        cust = get_user_by_ssn(customerSSN)
+        if cust is not None:
+            return f"User {cust.fullName} with ssn: {cust.customerSSN} already exists"
+        else:
+            cust = Customer(customerSSN=customerSSN, fullName=fullName, loanAmount=loanAmount, equityAmount=equityAmount, salaryAmount=salaryAmount)
+            db.session.add(cust)
+            db.session.commit()
+            return f"Successfully registered new user: {cust.fullName} with ssn: {cust.customerSSN}"
+    else:
+        return render_template('register.html')
